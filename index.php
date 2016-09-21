ff2d584b7cae94cdd7651132303586252bd02e97
Ademir Constantino <ademir.constantino@constantinoit.com>
2016-09-21 02:46:06 -0300

    ok

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-<?php
-$lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-if ($lang=portuguese) {
-    header("Location: http://www.constantinoit.com/ptbr");
-    } else if ($lang=spanish) {
-        header("Location: http://www.constantinoit.com/es");
-    } else {
-        header("Location: http://www.constantinoit.com/en");
-    }
+<?
+   $http_lang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);
+      switch ($http_lang) {
+        case 'en':
+         header("location:http://www.constantinoit.com/en/index.php");
+          break;
+        case 'es':
+           header("location:http://www.constantinoit.com/es/index.php");
+          break;
+	case 'pt':
+          header("location:http://www.constantinoit.com/ptbr/index.php");
+          break;
+        default:
+           header("location:http://www.constantinoit.com/en/index.php");
+      }
 ?>
